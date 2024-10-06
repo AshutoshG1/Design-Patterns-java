c8ffef9cb5aede000967ec9035336c8f19d1ed14
AshutoshG1 <53992254+AshutoshG1@users.noreply.github.com>
2024-10-06 21:04:35 +0530

    Update SingletonEager.java

--- src/DesignPatterns/Singleton/SingletonEager.java
+++ src/DesignPatterns/Singleton/SingletonEager.java
@@ -3,6 +3,10 @@ package DesignPatterns.Singleton;
 public class SingletonEager {
     private static SingletonEager instance = new SingletonEager();
 
+    private EagerSingleton() {
+        // Optional: Initialization code here
+    }
+
     public static SingletonEager getInstance(){
         return instance;
     }
